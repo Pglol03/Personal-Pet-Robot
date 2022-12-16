4d439d50da0286abac39a902fabba853c268c495
pglo03 <pradyungayakwad@gmail.com>
2022-12-16 10:05:52 +0530

    ir sensor not working

--- IR_Sensor.py
+++ IR_Sensor.py
@@ -4,30 +4,37 @@ import time
 
 IO.setwarnings(False)
 
-IO.setmode(IO.BCM)
+IO.setmode(IO.BOARD)
 
 
-IO.setup(27,IO.OUT) #GPIO 2 -> Red LED as output
+IO.setup(11,IO.OUT) #GPIO 2 -> Red LED as output
 
-IO.setup(22,IO.OUT) #GPIO 3 -> Green LED as output
+IO.setup(13,IO.OUT) #GPIO 3 -> Green LED as output
 
-IO.setup(17,IO.IN) #GPIO 14 -> IR sensor as input
+IO.setup(15,IO.IN) #GPIO 14 -> IR sensor Front as input
 
+IO.setup(16,IO.OUT) #GPIO 2 -> Red LED as output
 
+IO.setup(18,IO.OUT) #GPIO 3 -> Green LED as output
+
+IO.setup(22,IO.IN) #GPIO 14 -> IR sensor back as input
+# while 1:
+#     IO.output(11,True)
+#     IO.output(16,True)
 while 1:
 
 
-    if(IO.input(17)==True): #object is far away
+    if(IO.input(15)==True): #object is far away
+        print("case1")
+        IO.output(11,True) #Red led ON
 
-        IO.output(27,True) #Red led ON
-
-        IO.output(22,False) # Green led OFF
+        IO.output(13,False) # Green led OFF
 
     
 
-    if(IO.input(17)==False): #object is near
+    if(IO.input(15)==False): #object is near
 
-        IO.output(22,True) #Green led ON
+        IO.output(13,True) #Green led ON
 
-        IO.output(27,False) # Red led OFF
+        IO.output(11,False) # Red led OFF
 
