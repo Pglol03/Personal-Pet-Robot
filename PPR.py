f773a8a8c5f5f677da6a04c341456c7a3910544b
Pradyun Gayakwad <pradyungayakwad@gmail.com>
2024-04-22 17:00:13 +0530

    latest

--- PPR.py
+++ PPR.py
@@ -2,22 +2,44 @@ from Recognition import listen
 from Commands import action
 
 import threading
-from threading import Thread
-import Vars
 
+global queue
+class Queue(object):
+    def __init__(self):
+        self.item= []
+
+    def __repr__(self):
+        return "{}".format(self.item)
+
+    def __str__(self):
+        return "{}".format(self.item)
+
+    def enque(self, add):
+        self.item.insert(0, add)
+        return True
+
+    def size(self):
+        return len(self.item)
+
+    def isempty(self):
+        if self.size() == 0:
+             return True
+        else:
+             return False 
+    
+    def deque(self):
+        if self.size() == 0:
+            return None
+        else:
+            return self.item.pop()
+        
+queue = Queue()    
 
 listenT = threading.Thread(target=listen())
-commandT = threading.Thread(target=action, args=(Vars.queue.deque(),))
+commandT = threading.Thread(target=action, args=(queue.deque(),))
+
 listenT.start()
 listenT.join()
-
 commandT.start()
-while Vars.x:
-    # Thread.start_new_thread( listen)
-    # Thread.join()
-    # Thread.start_new_thread( commandT)
-    pass
-    
-
 # Arg = listen()
 # action(Arg)
